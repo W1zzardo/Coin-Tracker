b96d49cb2483de0d960bcd1c2d55d575d23922b4
Gregory C Slager <gregory.slager@student.uva.nl>
2018-01-24 13:59:51 +0000

    api.py had now fixed limit 1000

--- Website/api.py
+++ Website/api.py
@@ -4,25 +4,19 @@ from cs50 import SQL
 
 db = SQL("sqlite:///finance.db")
 api = "https://api.coinmarketcap.com/v1/ticker/?"
-limit = input("limit:")
-limit = int(limit)
 
+limit = 1000
 url = api + urllib.parse.urlencode({"limit": limit})
 json_data = requests.get(url).json()
+db.execute("DELETE FROM coins")
 
 for i in range(limit):
     naam = json_data[i]["id"]
     naam = str(naam)
     prijs = json_data[i]["price_usd"]
-   # prijs = float(prijs)
     cap = json_data[i]["market_cap_usd"]
-    #cap = float(cap)
     percent_1h = json_data[i]["percent_change_1h"]
-    #percent_1h = float(percent_1h)
     percent_24h = json_data[i]["percent_change_24h"]
-    #percent_24h = float(percent_24h)
     percent_7d = json_data[i]["percent_change_7d"]
-    #percent_7d = float(percent_7d)
 
-    #db.execute("INSERT INTO coins(naam) VALUES(:naam)", naam = naam)
     db.execute("INSERT INTO coins (naam, prijs, cap, percent_1h, percent_24h, percent_7d) VALUES(:naam, :prijs, :cap, :percent_1h, :percent_24h, :percent_7d)", naam = naam, prijs = prijs, cap = cap, percent_1h = percent_1h, percent_24h = percent_24h, percent_7d = percent_7d)
