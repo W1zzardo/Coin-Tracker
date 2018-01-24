dabf2885810bd78a4ad85a35d9577384442734a6
Gregory C Slager <gregory.slager@student.uva.nl>
2018-01-24 14:22:02 +0000

    added api.py to helpers.py

--- Website/helpers.py
+++ Website/helpers.py
@@ -1,5 +1,9 @@
 import csv
 import urllib.request
+import urllib.parse
+import requests
+from cs50 import SQL
+
 
 from flask import redirect, render_template, request, session
 from functools import wraps
@@ -112,3 +116,23 @@ def lookup(symbol):
 def usd(value):
     """Formats value as USD."""
     return f"${value:,.2f}"
+
+def api():
+    db = SQL("sqlite:///finance.db")
+    api = "https://api.coinmarketcap.com/v1/ticker/?"
+
+    limit = 500
+    url = api + urllib.parse.urlencode({"limit": limit})
+    json_data = requests.get(url).json()
+    db.execute("DELETE FROM coins")
+
+    for i in range(limit):
+        naam = json_data[i]["id"]
+        naam = str(naam)
+        prijs = json_data[i]["price_usd"]
+        cap = json_data[i]["market_cap_usd"]
+        percent_1h = json_data[i]["percent_change_1h"]
+        percent_24h = json_data[i]["percent_change_24h"]
+        percent_7d = json_data[i]["percent_change_7d"]
+
+        db.execute("INSERT INTO coins (naam, prijs, cap, percent_1h, percent_24h, percent_7d) VALUES(:naam, :prijs, :cap, :percent_1h, :percent_24h, :percent_7d)", naam = naam, prijs = prijs, cap = cap, percent_1h = percent_1h, percent_24h = percent_24h, percent_7d = percent_7d)
